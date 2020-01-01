21433063c67272d7921aa84b4b2d19c790d05c2c
Kitt3120 <Kitt3120@aol.de>
2020-01-01 18:44:53 +0100

    Removed generic condition from IDataProvider
    Removed where T : IConvertible generic condition from IDataProvider

--- Fehlzeitenerfassung/Fehlzeitenerfassung/IO/DataProvider/IDataProvider.cs
+++ Fehlzeitenerfassung/Fehlzeitenerfassung/IO/DataProvider/IDataProvider.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Fehlzeitenerfassung.IO.DataProvider
 {
-    interface IDataProvider<T> where T : IConvertible
+    interface IDataProvider<T>
     {
         T Provide(string[] args);
         Task<T> ProvideAsync(string[] args);
